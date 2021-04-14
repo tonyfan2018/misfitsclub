03ea436f2ea71c2de4b454f9d087e828b216723e
tonyfan <tonyfan2018@gmail.com>
2021-04-14 15:28:10 -0400

    asdfas

--- server.js
+++ server.js
@@ -47,10 +47,10 @@ app.use("/api/users", users);
 // process.env.port is Heroku's port if you choose to deploy the app there
 const port = process.env.PORT || 8000; 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('./client/build'));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
   
 
